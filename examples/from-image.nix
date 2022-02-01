11f286ab2b546d2d79af3dbe7d0ad9cc555b87df
Antoine Eiche <lewo@abesis.fr>
2022-02-01 21:36:14 +0100

    Fix examples.fromImage

--- examples/from-image.nix
+++ examples/from-image.nix
@@ -1,11 +1,11 @@
-{ pkgs }: let
-  alpine = pkgs.nix2container.pullImage {
+{ pkgs, nix2container }: let
+  alpine = nix2container.pullImage {
     imageName = "alpine";
     imageDigest = "sha256:115731bab0862031b44766733890091c17924f9b7781b79997f5f163be262178";
     sha256 = "sha256-o4GvFCq6pvzASvlI5BLnk+Y4UN6qKL2dowuT0cp8q7Q=";
   };
 in
-pkgs.nix2container.buildImage {
+nix2container.buildImage {
   name = "from-image";
   fromImage = alpine;
   config = {
