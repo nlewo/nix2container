9d907991bcd1e9b71768364201686233d06b216b
Antoine Eiche <lewo@abesis.fr>
2022-06-08 21:20:23 +0200

    examples.bash: fix typos

--- examples/bash.nix
+++ examples/bash.nix
@@ -3,9 +3,9 @@ nix2container.buildImage {
   name = "bash";
   contents = [
     # When we want tools in /, we need to symlink them in order to
-    # still have libraries in /nix/store. This differs from
-    # dockerTools.buildImage but this allows to avoid habing files
-    # both in / and /nix/store.
+    # still have libraries in /nix/store. This behavior differs from
+    # dockerTools.buildImage but this allows to avoid having files
+    # in both / and /nix/store.
     (pkgs.symlinkJoin { name = "root"; paths = [ pkgs.bashInteractive pkgs.coreutils ]; })
   ];
   config = {
