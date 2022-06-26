646c19b0bdc2cb46fb0c240660376956002a4334
David Arnold <david.arnold@iohk.io>
2022-06-25 20:51:19 -0500

    imp: add debug flag (to show popularity scores, etc.)

--- cmd/root.go
+++ cmd/root.go
@@ -3,8 +3,11 @@ package cmd
 import (
 	"os"
 
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
+
+var debug bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -22,4 +25,11 @@ func Execute() {
 }
 
 func init() {
+	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		if debug {
+			logrus.Info("Debug logs enabled")
+			logrus.SetLevel(logrus.DebugLevel)
+		}
+	}
+	rootCmd.PersistentFlags().BoolVarP(&debug, "debug", "d", false, "verbose logging")
 }
