649faf705f66d713af10846f0efbe57526af3164
Joshua Gilman <joshuagilman@gmail.com>
2022-09-21 11:30:44 -0700

    fix: updates test hash

--- examples/from-image.nix
+++ examples/from-image.nix
@@ -2,7 +2,7 @@
   alpine = nix2container.pullImage {
     imageName = "alpine";
     imageDigest = "sha256:115731bab0862031b44766733890091c17924f9b7781b79997f5f163be262178";
-    sha256 = "sha256-o4GvFCq6pvzASvlI5BLnk+Y4UN6qKL2dowuT0cp8q7Q=";
+    sha256 = "sha256-UHUcZBsoZ+DpAxroGPHkJ848sI9BF3iUia8RvUkfEC0=";
   };
 in
 nix2container.buildImage {
