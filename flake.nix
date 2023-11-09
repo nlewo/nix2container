79e6874a8ba066203e0352498a57b10396c65c8f
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-11-10 02:30:37 +0300

    fix: move overlays

--- flake.nix
+++ flake.nix
@@ -5,32 +5,33 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
   outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays.default = final: prev: {
-          nix2container = import ./. { system = final.system; pkgs = prev; };
-        };
-        
-        pkgs = inputs.nixpkgs.legacyPackages.${system}.extend overlays.default;
-        
-        examples = pkgs.callPackage ./examples { };
-        tests = pkgs.callPackage ./tests { inherit examples; };
+    inputs.flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; overlays = [ inputs.self.overlays.default ]; };
 
-        packages = {
-          inherit (pkgs.nix2container) nix2container-bin skopeo-nix2container;
-          inherit examples tests;
-          default = packages.nix2container-bin;
-          hello = pkgs.nix2container.buildImage {
-            name = "hello";
-            config = {
-              entrypoint = [ "${pkgs.hello}/bin/hello" ];
+          examples = pkgs.callPackage ./examples { };
+          tests = pkgs.callPackage ./tests { inherit examples; };
+
+          packages = {
+            inherit (pkgs.nix2container) nix2container-bin skopeo-nix2container;
+            inherit examples tests;
+            default = packages.nix2container-bin;
+            hello = pkgs.nix2container.buildImage {
+              name = "hello";
+              config = {
+                entrypoint = [ "${pkgs.hello}/bin/hello" ];
+              };
             };
           };
-        };
-      in
-      {
-        inherit packages;
-        inherit (pkgs) nix2container;
-        inherit overlays;
-      });
+        in
+        {
+          inherit packages;
+          inherit (pkgs) nix2container;
+        })
+    // {
+      overlays.default = final: prev: {
+        nix2container = import ./. { system = final.system; pkgs = prev; };
+      };
+    };
 }
