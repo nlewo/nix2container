a8ae066ffcf9087ad64fdd39b79397716b2ac15b
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-07-01 20:57:53 +0200

    Rewrite makeNixDatabase and closureGraph

--- default.nix
+++ default.nix
@@ -309,63 +309,46 @@ let
   # Also makes all these paths store roots to prevent them from being garbage collected.
   makeNixDatabase = closureGraphJson:
     assert l.isDerivation closureGraphJson;
-    pkgs.runCommand "nix-database" {}''
-      mkdir $out
+    pkgs.runCommand "nix-database" {
+      nativeBuildInputs = with pkgs; [ jq nix sqlite ];
+    } ''
       echo "Generating the nix database from ${closureGraphJson}..."
-      export NIX_REMOTE=local?root=$PWD
-      # A user is required by nix
-      # https://github.com/NixOS/nix/blob/9348f9291e5d9e4ba3c4347ea1b235640f54fd79/src/libutil/util.cc#L478
-      export USER=nobody
-      export PATH=${pkgs.jq.bin}/bin:${pkgs.sqlite}/bin:"$PATH"
+
       # Avoid including the closureGraph derivation itself.
-      # Transformation taken from https://github.com/NixOS/nixpkgs/blob/e7f49215422317c96445e0263f21e26e0180517e/pkgs/build-support/closure-info.nix#L33
-      jq -r 'map([.path, .narHash, .narSize, "", (.references | length)] + .references) | add | map("\(.)\n") | add' ${closureGraphJson} \
-        | head -n -1 \
-        | ${pkgs.nix}/bin/nix-store --load-db -j 1
+      # Transformation taken from https://github.com/NixOS/nixpkgs/blob/c22ce64ccddc6d59cb3747827d0417f8c10bd9cf/pkgs/build-support/closure-info.nix#L70
+      jq -r 'map([.path, .narHash, .narSize, "", (.references | length)] + .references) | add | map("\(.)\n") | add' ${closureGraphJson} |
+        head -n -1 |
+        NIX_REMOTE="local?root=$PWD" nix-store --load-db -j 1
 
       # Sanitize time stamps
-      sqlite3 $PWD/nix/var/nix/db/db.sqlite \
-        'UPDATE ValidPaths SET registrationTime = 0;';
+      sqlite3 $PWD/nix/var/nix/db/db.sqlite 'UPDATE ValidPaths SET registrationTime = 0;'
 
       # Dump and reimport to ensure that the update order doesn't somehow change the DB.
       sqlite3 $PWD/nix/var/nix/db/db.sqlite '.dump' > db.dump
       mkdir -p $out/nix/var/nix/db/
       sqlite3 $out/nix/var/nix/db/db.sqlite '.read db.dump'
-      mkdir -p $out/nix/store/.links
 
       mkdir -p $out/nix/var/nix/gcroots/docker/
       for i in $(jq -r 'map("\(.path)\n") | add' ${closureGraphJson}); do
         ln -s $i $out/nix/var/nix/gcroots/docker/$(basename $i)
-      done;
+      done
     '';
 
   # Write the references of `path' to a file but do not include `ignore' itself if non-null.
   closureGraph = paths: ignore:
-    let ignoreList =
-      if ignore == null
-      then []
-      else if !(builtins.isList ignore)
-      then [ignore]
-      else ignore;
-    in pkgs.runCommand "closure-graph.json"
-    {
+    let ignoreList = if ignore == null then [] else l.toList ignore;
+    in pkgs.runCommand "closure-graph.json" {
       exportReferencesGraph.graph = paths;
       __structuredAttrs = true;
-      PATH = "${pkgs.jq}/bin";
-      ignoreListJson = builtins.toJSON (builtins.map builtins.toString ignoreList);
-      outputChecks.out = {
-        disallowedReferences = ignoreList;
-      };
-      builder = l.toFile "builder"
-      ''
-        . .attrs.sh
-        jq --argjson ignore "$ignoreListJson" \
-          '.graph|map(select(.path as $p | $ignore | index($p) | not))|map(.references|=sort_by(.))|sort_by(.path)' \
-          .attrs.json \
-          > ''${outputs[out]}
-      '';
-    }
-    "";
+
+      ignoreListJson = l.toJSON (map toString ignoreList);
+      outputChecks.out.disallowedReferences = ignoreList;
+
+      nativeBuildInputs = [ pkgs.jq ];
+    } ''
+      jq '.graph | map(select(.path as $p | $ignore | index($p) | not)) | map(.references |= sort_by(.)) | sort_by(.path)' \
+        --argjson ignore "$ignoreListJson" .attrs.json > $out
+    '';
 
   buildImage = {
     name,
